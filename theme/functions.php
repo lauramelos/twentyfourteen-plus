4e28816eeecb94834e453a6da47a206334d010d1
Damian Suarez <rdsuarez@gmail.com>
2013-12-27 16:55:45 -0300

    don't load main.js script

--- theme/functions.php
+++ theme/functions.php
@@ -6,7 +6,6 @@
 
 function theme_scripts() {
   wp_enqueue_script( 'theme-core', get_stylesheet_directory_uri() . '/core.js' );
-  wp_enqueue_script( 'theme-main', get_stylesheet_directory_uri() . '/main.js' );
 }
 
 add_action( 'wp_enqueue_scripts', 'theme_scripts' );
