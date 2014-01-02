a2c8c9e775eba48e19ebefd36daebc2337a2fd51
Damian Suarez <rdsuarez@gmail.com>
2014-01-02 09:55:33 -0300

    update single template support

--- theme/single.php
+++ theme/single.php
@@ -1,47 +1,47 @@
-<?php if (!isAjax()) : ?>
-
 <?php
 /**
- * The Template for displaying all single posts
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme and one
+ * of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query,
+ * e.g., it puts together the home page when no home.php file exists.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
  *
  * @package WordPress
  * @subpackage Twenty_Fourteen
  * @since Twenty Fourteen 1.0
  */
 
-get_header(); ?>
-<?php endif; ?>
-
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
-			<?php
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
-
-					/*
-					 * Include the post format-specific template for the content. If you want to
-					 * use this in a child theme, then include a file called called content-___.php
-					 * (where ___ is the post format) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-
-					// Previous/next post navigation.
-					twentyfourteen_post_nav();
-
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
-						comments_template();
-					}
-				endwhile;
-			?>
-		</div><!-- #content -->
-	</div><!-- #primary -->
-<?php if (!isAjax()) : ?>
-
-<?php
-get_sidebar( 'content' );
-get_sidebar();
-get_footer();
+get_template_part('head', 'head');
 ?>
 
-<?php endif; ?>
+<body <?php body_class(); ?> data-config='<?php echo json_encode($config); ?>'>
+  <div id="page" class="hfeed site">
+    <?php get_header(); ?>
+
+    <div id="main" class="site-main">
+      <div id="main-content" class="main-content">
+        <?php
+        if ( is_front_page() && twentyfourteen_has_featured_posts() ) {
+          // Include the featured content template.
+          get_template_part( 'featured-content' );
+        }
+        ?>
+
+        <div id="primary" class="content-area">
+          <?php get_template_part('./partials/' . get_current_theme_tpl()) ?>
+        </div><!-- #primary -->
+
+        <?php get_sidebar( 'content' ); ?>
+        </div><!-- #main-content -->
+
+        <div id="secondary"></div>
+
+      </div><!-- #main -->
+
+      <footer id="colophon" class="site-footer" role="contentinfo"></footer>
+	    <?php wp_footer(); ?>
+    </div><!-- #page -->
+</body>
