c1fd5149b54e204e07ba41de5844f52b8e687c07
Damian Suarez <rdsuarez@gmail.com>
2014-01-02 13:16:17 -0300

    load content-sidebar through async request

--- theme/index.php
+++ theme/index.php
@@ -12,24 +12,25 @@
 get_template_part('head', 'head');
 ?>
 
-<body <?php body_class(); ?> data-config='<?php echo json_encode($config); ?>'>
+<body <?php body_class(); ?>>
   <div id="page" class="hfeed site">
     <?php get_header(); ?>
 
     <div id="main" class="site-main">
       <div id="main-content" class="main-content">
-        <?php
-        if ( is_front_page() && twentyfourteen_has_featured_posts() ) {
+        <?php if ( is_front_page() && twentyfourteen_has_featured_posts() ) {
           // Include the featured content template.
-          get_template_part( 'featured-content' );
-        }
-        ?>
+          //get_template_part( 'featured-content' );
+        } ?>
 
         <div id="primary" class="content-area">
           <?php get_template_part('./partials/' . get_current_theme_tpl()) ?>
         </div><!-- #primary -->
 
-        <?php get_sidebar( 'content' ); ?>
+        <?php if (is_active_sidebar( 'sidebar-2' ) ) : ?>
+          <div id="content-sidebar" class="content-sidebar widget-area" role="complementary">
+          </div><!-- #content-sidebar -->
+        <?php endif; ?>
 
       </div><!-- #main-content -->
 
