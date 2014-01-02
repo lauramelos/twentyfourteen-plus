540018e2dabceaab5c11191ec44d90783c431925
Damian Suarez <rdsuarez@gmail.com>
2014-01-02 08:42:37 -0300

    refact -> render sections using partial tpls

--- theme/index.php
+++ theme/index.php
@@ -1,19 +1,12 @@
-<?php if (!isAjax()) : ?>
-
 <?php
 /**
  * The main template file
  *
- * This is the most generic template file in a WordPress theme and one
- * of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query,
- * e.g., it puts together the home page when no home.php file exists.
- *
- * @link http://codex.wordpress.org/Template_Hierarchy
+ * Main template
  *
  * @package WordPress
- * @subpackage Twenty_Fourteen
- * @since Twenty Fourteen 1.0
+ * @subpackage Twenty_Fourteen_Plus
+ * @since Twenty Fourteen Plus 1.0
  */
 
 get_template_part('head', 'head');
@@ -25,49 +18,26 @@ get_template_part('head', 'head');
 
     <div id="main" class="site-main">
       <div id="main-content" class="main-content">
-      <?php
+        <?php
         if ( is_front_page() && twentyfourteen_has_featured_posts() ) {
           // Include the featured content template.
           get_template_part( 'featured-content' );
         }
-      ?>
-      <?php endif; ?>
+        ?>
 
         <div id="primary" class="content-area">
-          <div id="content" class="site-content" role="main">
-          <?php
-            if ( have_posts() ) :
-              // Start the Loop.
-              while ( have_posts() ) : the_post();
-
-                /*
-                 * Include the post format-specific template for the content. If you want to
-                 * use this in a child theme, then include a file called called content-___.php
-                 * (where ___ is the post format) and that will be used instead.
-                 */
-                get_template_part( 'content', get_post_format() );
-
-              endwhile;
-              // Previous/next post navigation.
-              twentyfourteen_paging_nav();
-
-            else :
-              // If no content, include the "No posts found" template.
-              get_template_part( 'content', 'none' );
-
-            endif;
-          ?>
-          </div><!-- #content -->
-
+          <?php get_template_part('./partials/' . get_current_theme_tpl()) ?>
         </div><!-- #primary -->
-      <?php if (!isAjax()) : ?>
-      <?php get_sidebar( 'content' ); ?>
+
+        <?php get_sidebar( 'content' ); ?>
+
       </div><!-- #main-content -->
 
       <div id="secondary"></div>
-      </div><!-- #main -->
 
-      <footer id="colophon" class="site-footer" role="contentinfo"></footer>
-	    <?php wp_footer(); ?>
-    </div><!-- #page -->
-  <?php endif; ?>
+    </div><!-- #main -->
+
+    <footer id="colophon" class="site-footer" role="contentinfo"></footer>
+	  <?php wp_footer(); ?>
+  </div><!-- #page -->
+</body>
